68563ecdad0360b669677ffa25b3261e4be79dff
junhonglim49791 <limjunhong4979@gmail.com>
2025-06-15 11:24:45 +0800

    Spawn asteroids

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
@@ -12,6 +14,13 @@ def main():
     drawable = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    asteroids = pygame.sprite.Group()
+    Asteroid.containers = (asteroids, updatable, drawable)
+    
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+
 
 
     while True:
