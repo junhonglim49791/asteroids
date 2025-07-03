01ef1db7101bd868d622b320bd5e2f84e9b955c5
junhonglim49791 <limjunhong4979@gmail.com>
2025-07-03 16:18:51 +0800

    Shield power up

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import pygame
 import sys
+import random
 from constants import *
-from player import Player
+from player import Player, Shield
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
 from bullets import Shot
 
+
 def main():
+    # Pygame variables
     pygame.init()
-    game_over = False
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
@@ -16,7 +18,10 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
+    # Since shield is instantiate in in Player, i need to add this before creating player object, else its not added into the containers
+    Shield.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
 
     asteroids = pygame.sprite.Group()
     Asteroid.containers = (asteroids, updatable, drawable)
@@ -27,6 +32,9 @@ def main():
     bullets = pygame.sprite.Group()
     Shot.containers = (bullets, updatable, drawable)
 
+    # Game logic variables
+    game_over = False
+    shield = None
 
     while True:
         for event in pygame.event.get():
@@ -45,7 +53,30 @@ def main():
             # Doesn't work because Group.draw() in sprite module expects "image" attribute
             # drawable.draw(screen)
 
+            # Awards shield power up for player accuracy. 
+            # shield is None prevent more than 1 shield in game but doesnt prevent shield to be generated when player in shield power up
+            # player.shield is None make sure no shield can be generated when player reach shield combo streak during shield power up
+            if player.shield_combo_streak == 3 and shield is None and player.shield is None:
+                x = random.randint(SHIELD_RADIUS, SCREEN_WIDTH - SHIELD_RADIUS)
+                y = random.randint(SHIELD_RADIUS, SCREEN_HEIGHT - SHIELD_RADIUS)
+                shield = Shield(x, y, SHIELD_RADIUS)
+                player.shield_combo_streak = 0
+            
+            # Check whether player gets the game shield. If so, remove game shield and player now has a shield
+            if shield and shield.is_collided(player):
+                shield.kill()
+                shield = None
+                player.shield = Shield(player.position.x, player.position.y, SHIELD_RADIUS)
+                player.invincible = 5
+            
+            # Remove player shield when timer expires
+            if player.invincible < 0 and player.shield is not None:
+                player.shield.kill()
+                player.shield = None
+
             for asteroid in asteroids:
+                asteroid.wrap_around()
+
                 if asteroid.is_collided(player) and player.invincible < 0:
                     player.life -= 1
                     if player.life < 0:
@@ -58,19 +89,27 @@ def main():
                     # 5s invincibility after respawn
                     player.invincible = 5
                     
-                asteroid.wrap_around()
-                    
+                    player.shield_combo_streak = 0
+
+                # Shield power up kill asteroids like bullets
+                if player.shield is not None and asteroid.is_collided(player.shield):
+                    asteroid.split()
+                    player.score = max(player.score + 1, player.streak + player.score)
+                    player.streak += 1
+                        
                 for bullet in bullets:
                     if asteroid.is_collided(bullet):
                         bullet.kill()
                         asteroid.split()
                         player.score = max(player.score + 1, player.streak + player.score)
                         player.streak += 1
-
+                        if player.shield_combo_streak < 3:
+                            player.shield_combo_streak += 1
                     elif bullet.is_out_of_bounds():
                         bullet.kill()
-                        player.streak = 0           
-        else:
+                        player.streak = 0 
+                        player.shield_combo_streak = 0          
+        else:           
             player.show_highest_score(screen)
 
         pygame.display.flip()
--- player.py
+++ player.py
@@ -2,7 +2,38 @@ import pygame
 from circleshape import CircleShape
 from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED, SHOT_RADIUS, SCREEN_WIDTH, SCREEN_HEIGHT
 from bullets import Shot
+import math
 
+class Shield(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+        # I don't have to add position during instantiation, can add all these circles with Shield's current position
+        self.dotted_circles = [pygame.Vector2(math.cos(math.radians(30)*i), math.sin(math.radians(30)*i)) * self.radius for i in range(0, 12)]
+        # self.base_vectors = [
+        #     pygame.Vector2(0, self.radius).rotate(i * 360 / 12)  # 12 dots
+        #     for i in range(12)
+        # ]
+        self.rotation_angle = 0
+
+    # def draw(self, screen):
+    #     for vec in self.base_vectors:
+    #         rotated_vec = vec.rotate(self.rotation_angle)
+    #         pos = self.position + rotated_vec
+    #         pygame.draw.circle(screen, color="white", center=pos, radius=2, width=2)
+
+    def draw(self, screen):
+        for circle in self.dotted_circles:
+            # if i passed in rotated_circle to draw.circle(), the circle is rotated relative to origin (top left corner of the game window)
+            rotated_circle = circle.rotate(self.rotation_angle)
+            # all these dotted circles should always be relative to the shield's current position
+            rotated_position = self.position + rotated_circle
+            pygame.draw.circle(screen, color="white", center=rotated_position, radius=2, width=2)
+    
+    def update(self, dt):
+        self.rotation_angle += 30*dt
+
+    def is_collided(self, circle):
+        return super().is_collided(circle)
 
 class Player(CircleShape):
     def __init__(self, x, y):
@@ -13,6 +44,8 @@ class Player(CircleShape):
         self.streak = 0
         self.life = 3
         self.invincible = 0
+        self.shield = None
+        self.shield_combo_streak = 0
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -30,25 +63,31 @@ class Player(CircleShape):
             return [[a, b, c], [d, e, f]]  
 
         return [[a,b,c]]
-        
+
     def draw(self, screen):
         score = pygame.font.Font(None, 36).render(f"Score: {str(self.score)}", True, "white")
         streak = pygame.font.Font(None, 36).render(f"{str(self.streak)}x Combo", True, "white")
         lives = pygame.font.Font(None, 36).render(f"{str(self.life)} Live(s)", True, "white")
+        shield_combo = pygame.font.Font(None, 36).render(f"Shield {str(self.shield_combo_streak)}/3", True, "white")
         screen.blit(score, (5, 5))
         screen.blit(streak, (5, 40))
         screen.blit(lives, (5, 75))
-        
+        screen.blit(shield_combo, (5, 110))
+
+        # self.shield.draw(screen)
+
+        pygame.draw.polygon(screen, color="white", points=self.triangle()[0], width=2)  
+        if self.invincible > 0 and self.shield is None:
+            pygame.draw.polygon(screen, color="white", points=self.triangle()[1], width=2)   
+
         # Check real shape and triangle shape difference
         # pygame.draw.circle(screen, color="white", center=self.position, radius=self.radius, width=2)
-        pygame.draw.polygon(screen, color="white", points=self.triangle()[0], width=2)  
-        if self.invincible > 0:
-            pygame.draw.polygon(screen, color="white", points=self.triangle()[1], width=2)        
+        # pygame.draw.polygon(screen, color="white", points=self.triangle()[0], width=2)     
       
 
     def show_highest_score(self, screen):
         highest_score = pygame.font.Font(None, 80).render(f"Nice try! Your Score: {self.score}", True, "white")
-        screen.blit(highest_score, (SCREEN_WIDTH/4, SCREEN_HEIGHT/3))
+        screen.blit(highest_score, (SCREEN_WIDTH/6, SCREEN_HEIGHT/3))
 
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
@@ -70,6 +109,7 @@ class Player(CircleShape):
         
         if keys[pygame.K_SPACE]:
             self.shoot(dt)
+
     
     def move(self, dt):
         # +ve rotate clockwise, vice versa
@@ -77,6 +117,10 @@ class Player(CircleShape):
         # += forward works, but doesn't make it frame independent
         self.position += forward * PLAYER_SPEED * dt
 
+        # player shield has to keep track of player position to always display correctly around player
+        if self.shield is not None:
+            self.shield.position = self.position
+        
     def back_to_center(self):
         self.position = pygame.Vector2(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
         
